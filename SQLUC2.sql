81ce5844e8a683ef4bed9870cb1914b2c3fe41b0
imohtesham <ibraahimm92@gmail.com>
2022-12-30 19:39:16 +0530

    Create table AddressBook in My sql

---
+++ SQLUC2.sql
@@ -0,0 +1,12 @@
+use addressbook_services;
+CREATE TABLE AddressBook(
+FirstName varchar(50),
+LastName varchar(50),
+Address varchar(150),
+City char(50),
+State char(50),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(50)
+
+);
