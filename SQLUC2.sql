5350fcd4a41ff09e88ee935cc96b0a43d2e779ed
imohtesham <ibraahimm92@gmail.com>
2022-12-30 20:20:11 +0530

    ability to alter table With Type  AddressBook in My sql

--- SQLUC2.sql
+++ SQLUC2.sql
@@ -7,7 +7,7 @@ VALUES('mohd','ibrahim','7th cross','hhyd','telangana',500065,5243454345,'abc@gm
 UPDATE AddressBook set PhoneNumber=1234567890 where FirstName='Sai';
 UPDATE AddressBook set State='Maharastra',City='Mumbai' where FirstName='Mahesh';
 Delete From AddressBook where LastName='nath';
-select * from AddressBook;
+
 INSERT INTO AddressBook
 Values('shankar','rana','9th main','Bangaluru','Karnataka',560087,1234567897,'jkl@gmail.com'),
 ('karan','Kumar','10th main','Bangaluru','Karnataka',560097,1234567807,'mno@gmail.com'),
@@ -19,4 +19,16 @@ SELECT FirstName,City from AddressBook ORDER BY FirstName;
 
 SELECT * from AddressBook ORDER BY FirstName;
 SELECT * from AddressBook ORDER BY City;
+
+ALTER TABLE AddressBook
+ADD Type Varchar(200);
+
+Select * from AddressBook;
+Insert into AddressBook(FirstName,LastName ,Address,City,State,Zip,PhoneNumber,Email) 
+Values('Vagisha','Vignesh','12th main','Bengaluru','Karnataka',456765,9876545678,'stu@gmail.com') ;
+
+UPDATE AddressBook set Type='Friends' where FirstName='Gagana';
+UPDATE AddressBook set Type='Proffession' where FirstName='karan';
+UPDATE AddressBook set Type='Friends' where FirstName='hasini';
+UPDATE AddressBook set Type='Profession' where FirstName='Shankar';
 
