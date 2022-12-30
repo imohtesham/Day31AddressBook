ec35eb4fe0b715fcfb6ff39a4da7b6ec3e09a57f
imohtesham <ibraahimm92@gmail.com>
2022-12-30 20:07:59 +0530

    ability to restore person AddressBook in My sql

--- SQLUC2.sql
+++ SQLUC2.sql
@@ -8,4 +8,10 @@ UPDATE AddressBook set PhoneNumber=1234567890 where FirstName='Sai';
 UPDATE AddressBook set State='Maharastra',City='Mumbai' where FirstName='Mahesh';
 Delete From AddressBook where LastName='nath';
 select * from AddressBook;
+INSERT INTO AddressBook
+Values('shankar','rana','9th main','Bangaluru','Karnataka',560087,1234567897,'jkl@gmail.com'),
+('karan','Kumar','10th main','Bangaluru','Karnataka',560097,1234567807,'mno@gmail.com'),
+('hasini','renyu','11th main','Hydrabad','Telangana',561237,1653267897,'pqr@gmail.com');
+
+SELECT FirstName From AddressBook where  City='Bangaluru' or State='Karnataka';
 
