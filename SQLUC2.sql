ab4278ac1bffba38ba459000dc5f2fd46c0c9ade
imohtesham <ibraahimm92@gmail.com>
2022-12-30 19:50:00 +0530

    edit existing Contacts AddressBook in My sql

--- SQLUC2.sql
+++ SQLUC2.sql
@@ -14,4 +14,8 @@ VALUES('mohd','ibrahim','7th cross','hhyd','telangana',500065,5243454345,'abc@gm
 ('Mahesh','Koder','8th cross','Mysore','Karnataka',560066,1323567775,'def@gmail.com'),
 ('haneef','Gowri','9th cross','Hydrabad','Telangana',500095,5478908765,'ghi@gmail.com'),
 ('Sai','nath','4th cross','Jaipur','Rajasthan',660065,5243458745,'jkl@gmail.com');
+UPDATE AddressBook set PhoneNumber=1234567890 where FirstName='Sai';
+UPDATE AddressBook set State='Maharastra',City='Mumbai' where FirstName='Mahesh';
+
+select * from AddressBook;
 
